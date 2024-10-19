7112a6e9abfc35dc4a0b1353d85dc25d9e4fe023
Juan José Villarreal <jjvillarreal@MacBook-Air-de-Juan.local>
2024-10-20 00:07:19 +0200

    ShotenApp: Added some styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout ({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -18,17 +18,17 @@ export default function Home () {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Acortar URL</h1>
-        <form onSubmit={handleSubmit}>
-          <input
+    <div className="flex min-h-screen items-center justify-center font-[family-name:var(--font-geist-sans)]">
+      <main className="flex w-1/2 h-96 flex-col items-center justify-between py-24 sm:items-center ">
+        <h1 className='flex text-5xl opacity-90 text-indigo-600'>Shorten&nbsp;<span className='gradient-underline'>URL</span></h1>
+        <form className='w-full flex flex-col items-center justify-center' onSubmit={handleSubmit}>
+          <input className='flex text-center text-indigo-700 rounded-full py-3 mb-4 w-3/4 placeholder-pink-400'
             type="text"
-            placeholder="Ingresa la URL"
+            placeholder="Insert an URL"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
           />
-          <button type="submit">Acortar</button>
+          <button className='transition duration-300 justify-center flex text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold dark:focus:ring-blue-800 px-10 py-4 text-center me-2 mb-2 rounded-full' type="submit">Shorten</button>
         </form>
         {shortUrl && (
           <p>URL acortada: <a href={shortUrl} target='_blank' rel="noopener noreferrer">{shortUrl}</a></p>
